c7ae9a387e04cff0f1e5e6e43449517acf870d97
david galvan <galvan.dsierra@gmail.com>
2026-02-18 18:27:52 -0500

    sort  archive.html

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,6 @@
 import { BookmarkManagerFacade } from '@/core/bookmark-manager-facade';
-import { FileHandler } from '@/utils/file-handler';
-
 const manager = new BookmarkManagerFacade();
 
-let currentFiles = await manager.getFilesInDirectory('data/prev data/');
-currentFiles = currentFiles.filter((file: string) => file.endsWith('json'));
-
-const fileHandler = new FileHandler();
-
-const array = [];
-
-for (const file of currentFiles) {
-  const content = await fileHandler.read(file);
-  const data = JSON.parse(content);
-  array.push(data);
-
-  await fileHandler.delete(file);
-}
-
-const arrayFlat = array.flat();
-await fileHandler.write('data/twitter.json', JSON.stringify(arrayFlat));
+const newData = await manager.load('data/pending/archive.html');
+newData.orderBookmarksByDomain();
+await newData.saveBookmarks();
