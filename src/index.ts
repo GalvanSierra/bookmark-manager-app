fe128fb2052bd27f935d897f335bc38e9a9ec7c0
David Galvan S. <galvan.dsierra@gmail.com>
2026-02-18 22:01:04 +0000

    merge all twitter.json

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,22 @@
 import { BookmarkManagerFacade } from '@/core/bookmark-manager-facade';
+import { FileHandler } from '@/utils/file-handler';
 
 const manager = new BookmarkManagerFacade();
 
-const ultimateFile = await manager.load('data//ultimate_file.html');
-const oldFile = await manager.load('data//old_data.html');
+let currentFiles = await manager.getFilesInDirectory('data/prev data/');
+currentFiles = currentFiles.filter((file: string) => file.endsWith('json'));
 
-let currentFiles = await manager.getFilesInDirectory('data//original data/');
-currentFiles = currentFiles.filter((file: string) => file.endsWith('html'));
+const fileHandler = new FileHandler();
+
+const array = [];
 
 for (const file of currentFiles) {
-  const bookmarkManager = await manager.load(file);
-  ultimateFile.addBookmarks(bookmarkManager.getAllBookmarks());
-  await bookmarkManager.deleteFile();
+  const content = await fileHandler.read(file);
+  const data = JSON.parse(content);
+  array.push(data);
+
+  await fileHandler.delete(file);
 }
 
-let oldFiles = await manager.getFilesInDirectory('data//prev data/');
-oldFiles = oldFiles.filter((file: string) => file.endsWith('html'));
-
-for (const file of oldFiles) {
-  const bookmarkManager = await manager.load(file);
-  oldFile.addBookmarks(bookmarkManager.getAllBookmarks());
-  await bookmarkManager.deleteFile();
-}
-
-const currentBookmarks = ultimateFile.getAllBookmarks()
-const currentUrls = currentBookmarks.map(b => b.url)
-
-const bookmarksToDelete = oldFile.getAllBookmarks().filter(b => {
-  return currentUrls.includes(b.url)
-})
-oldFile.deleteBookmarks(bookmarksToDelete)
-
-const updatedBookmarks = oldFile.getAllBookmarks().map((b) => {
-  b.folder = '🏚️ old > ' + b.folder;
-  return b;
-});
-oldFile.updateBookmarks(updatedBookmarks);
-
-await ultimateFile.saveBookmarks();
-await oldFile.saveBookmarks();
+const arrayFlat = array.flat();
+await fileHandler.write('data/twitter.json', JSON.stringify(arrayFlat));
