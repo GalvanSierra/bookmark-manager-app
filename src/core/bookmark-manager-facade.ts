86bc8df2dbdd976cd2adef10109236250d926b83
david galvan <galvan.dsierra@gmail.com>
2026-02-18 14:07:40 -0500

    union current data

--- src/core/bookmark-manager-facade.ts
+++ src/core/bookmark-manager-facade.ts
@@ -1,6 +1,24 @@
 import { BookmarkManager } from '@/core/bookmark-manager';
 
+import { readdir } from 'node:fs/promises';
+
+// read all the files in the current directory, recursively
 export class BookmarkManagerFacade {
+  async getFilesInDirectory(path: string) {
+    const files = await readdir(path, { recursive: true });
+    return files
+      .filter((file) => {
+        const fileName = file.slice(file.lastIndexOf('\\') + 1);
+        if (!fileName.includes('.')) {
+          return false;
+        }
+        return true;
+      })
+      .map((b) => {
+        return path + b;
+      });
+  }
+
   async load(path: string): Promise<BookmarkManager> {
     const bookmarkManager = new BookmarkManager(path);
     await bookmarkManager.loadBookmarks();
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('Hello via Bun!');
+import { BookmarkManagerFacade } from '@/core/bookmark-manager-facade';
+
+const manager = new BookmarkManagerFacade();
+
+const ultimateFile = await manager.load('data//ultimate_file.html');
+
+let files = await manager.getFilesInDirectory('data//original data/');
+files = files.filter((file) => file.endsWith('html'));
+console.log(files);
+
+for (const file of files) {
+  const bookmarkManager = await manager.load(file);
+  ultimateFile.addBookmarks(bookmarkManager.getAllBookmarks());
+  await bookmarkManager.deleteFile();
+}
+
+await ultimateFile.saveBookmarks();
